7db8bd33db50cdd6cb7b32d17024806375eafb1e
Hossam Megahed <hosammegahed91@gmail.com>
2022-06-27 09:48:26 +0400

    Reverese strin

---
+++ unit1_class2_reverse_string.py
@@ -0,0 +1,13 @@
+"""
+
+
+"""
+import unit1_class1_stack as stack
+
+string = "gninraeL nIdekniL htiw tol a nraeL"
+reversed_string = ""
+s = stack.Stack()
+
+# Your solution here.
+
+print(reversed_string)
