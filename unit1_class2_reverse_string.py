357467793d16671e41d716958aeaa1f5ed0c1fad
Hossam Megahed <hosammegahed91@gmail.com>
2022-06-27 10:34:51 +0400

    r

--- unit1_class2_reverse_string.py
+++ unit1_class2_reverse_string.py
@@ -1,13 +1,13 @@
-"""
-
-
-"""
 import unit1_class1_stack as stack
 
 string = "gninraeL nIdekniL htiw tol a nraeL"
 reversed_string = ""
 s = stack.Stack()
 
-# Your solution here.
+for char in string:
+    s.push(char)
+
+while not s.is_empty():
+    reversed_string = reversed_string + s.pop()
 
 print(reversed_string)
